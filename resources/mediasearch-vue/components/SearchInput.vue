9c964e84db6b527aa6f0eb0c1f1aefa7e7fc5d0e
Eric Gardner <egardner@wikimedia.org>
2020-07-02 13:40:58 -0700

    Improve search input markup and styles
    Change-Id: I951bc80a2679dd0f798bb96cfd8a48de8b35f686

--- resources/mediasearch-vue/components/SearchInput.vue
+++ resources/mediasearch-vue/components/SearchInput.vue
@@ -1,12 +1,23 @@
 <template>
-	<div>
+	<div class="wbmi-media-search-input">
 		<input
 			v-model="term"
-			type="text"
+			class="wbmi-media-search-input__input"
+			type="search"
+			tabindex="0"
+			aria-disabled="false"
 			@keyup.enter="updateTerm"
 		>
 
+		<span class="wbmi-media-search-input__icon">
+			<mw-icon
+				:icon="'search'"
+				:invert="false"
+			></mw-icon>
+		</span>
+
 		<mw-button
+			class="wbmi-media-search-input__button"
 			:primary="true"
 			:progressive="true"
 			@click="updateTerm"
@@ -17,13 +28,15 @@
 </template>
 
 <script>
-var Button = require( './base/Button.vue' );
+var Button = require( './base/Button.vue' ),
+	Icon = require( './base/Icon.vue' );
 
 module.exports = {
 	name: 'SearchInput',
 
 	components: {
-		'mw-button': Button
+		'mw-button': Button,
+		'mw-icon': Icon
 	},
 
 	props: {
@@ -43,3 +56,49 @@ module.exports = {
 	}
 };
 </script>
+
+<style lang="less">
+@import 'mediawiki.mixins';
+@import '../../../lib/wikimedia-ui-base.less';
+
+.wbmi-media-search-input {
+	.flex-display();
+	.flex-wrap( nowrap );
+	box-sizing: border-box;
+	max-width: @max-width-base;
+	padding: 0 0 16px 0;
+	position: relative;
+	width: 100%;
+
+	&__input {
+		border: @border-style-base @border-width-base @border-color-base;
+		border-radius: @border-radius-base 0 0 @border-radius-base;
+		font-size: 14px;
+		padding: 6px;
+		padding-left: 36px;
+		width: 100%;
+
+		&:focus {
+			border-color: @color-primary--focus;
+			box-shadow: @box-shadow-base--focus;
+			outline: 0;
+		}
+
+		&::placeholder {
+			color: @color-placeholder;
+		}
+	}
+
+	&__button {
+		border-radius: 0 @border-radius-base @border-radius-base 0;
+	}
+
+	&__icon {
+		left: 3px;
+		opacity: 0.67;
+		padding: 5px;
+		position: absolute;
+	}
+}
+
+</style>
