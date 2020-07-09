70060d8c0dae82160c6070d67216a03f792ba66f
Eric Gardner <gardner.ec@gmail.com>
2020-07-09 12:50:53 -0700

    Vue.js MediaSearch UI
    Ports the MediaSearch front-end to Vue.js.
    
    Bug: T251940

---
+++ resources/mediasearch-vue/components/GenericResult.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="wbmi-generic-result">
+		<h3>
+			<a :href="canonicalurl" target="blank">
+				{{ title }}
+			</a>
+		</h3>
+	</div>
+</template>
+
+<script>
+// @vue/component
+module.exports = {
+	name: 'GenericResult',
+
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+
+		canonicalurl: {
+			type: String,
+			required: true
+		},
+
+		pageid: {
+			type: Number,
+			required: true
+		},
+
+		index: {
+			type: Number,
+			required: true
+		}
+	}
+};
+</script>
+
+<style lang="less">
+@import 'mediawiki.mixins';
+@import '../../../lib/wikimedia-ui-base.less';
+
+.wbmi-generic-result {
+	padding: 8px;
+
+	&:nth-of-type( odd ) {
+		background-color: #f6f8fc;
+	}
+
+	h3 {
+		padding-bottom: 0.5em;
+	}
+}
+</style>
---
+++ resources/mediasearch-vue/components/ImageResult.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="wbmi-image-result">
+		<a :href="canonicalurl"
+			@click.prevent="showDetails">
+			<img :src="thumbnail" :alt="title">
+		</a>
+	</div>
+</template>
+
+<script>
+var searchResult = require( '../mixins/searchResult.js' );
+
+module.exports = {
+	name: 'ImageResult',
+
+	mixins: [ searchResult ],
+
+	computed: {
+		width: function () {
+			return this.imageinfo[ 0 ].width;
+		},
+
+		height: function () {
+			return this.imageinfo[ 0 ].height;
+		},
+
+		aspectRatio: function () {
+			return this.width / this.height;
+		}
+	}
+
+};
+</script>
+
+<style lang="less">
+@import 'mediawiki.mixins';
+@import '../../../lib/wikimedia-ui-base.less';
+
+.wbmi-image-result {
+	box-sizing: border-box;
+	margin: 8px;
+
+	&:hover {
+		.box-shadow( 4px 4px 5px -2px @border-color-base );
+	}
+
+	img {
+		height: 180px;
+		min-width: 100px;
+		object-fit: cover;
+		object-position: center center;
+		width: 100%;
+	}
+
+	&__title {
+		height: 20px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+}
+</style>
